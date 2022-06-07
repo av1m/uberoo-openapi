d4d099d2661b8304eec20560b64aab355e59022b
av1m <36456709+av1m@users.noreply.github.com>
2022-06-07 14:08:27 +0200

    use docker-compose for swagger-ui

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
-const fs = require("fs");
+const chokidar = require("chokidar");
 
 const { exec } = require("child_process");
 
-fs.watch("src", function (event, filename) {
-  if (!filename || !event) return;
-  console.log(`${event}: ${filename}`);
+function rebuild(event, path) {
+  if (!path || !event) return;
+  console.log(`${event}: ${path}`);
   console.log("Compiling...");
   exec("npm run build", (err, stdout, stderr) => {
     if (err) console.error(err);
     if (stderr) console.error(stderr);
     console.log("Compiled!");
   });
-});
+}
+
+chokidar
+  .watch("src", {
+    ignoreInitial: true,
+  })
+  .on("add", (path) => rebuild("add", path))
+  .on("change", (path) => rebuild("change", path))
+  .on("unlink", (path) => rebuild("unlink", path));
